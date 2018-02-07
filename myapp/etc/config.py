3b7a64ab78e4148d23804f9d3ee1878127f788fd
qihengshan <qihengshan@chehejia.com>
2018-02-07 12:50:34 +0800

    add grant privileges

---
+++ myapp/etc/config.py
@@ -0,0 +1,28 @@
+#encoding:utf-8
+wrong_msg="select '请检查输入语句'"
+select_limit=200
+export_limit=200
+incp_host="xxxx"
+public_user="public"
+incp_port=6669
+incp_user="root"
+incp_passwd=""
+sqladvisor_switch = 0
+sqladvisor = '/usr/sbin/sqladvisor'
+pt_tool = 1
+pt_tool_path = '/usr/local/bin/'
+incept_backup_host = 'xxxxx'
+incept_backup_port = '3306'
+incept_backup_user = 'xxxxx'
+incept_backup_passwd = 'xxxxxx'
+path_to_mysqldiff = "/usr/local/bin/mysqldiff"
+opsdbpwd='xxxxxxxxx'
+opsdbuser='xxxx'
+dbamails=['xxxxxx']
+#数据库权限列表
+MYSQL_PRIVS = [
+    'ALL', 'SELECT', 'UPDATE', 'INSERT', 'DELETE', 'ALTER', 'CREATE', 'INDEX', 'DROP',
+    'PROCESS', 'RELOAD', 'REPLICATION CLIENT', 'REPLICATION SLAVE', 'USAGE', 'REFERENCES',
+    'CREATE TEMPORARY TABLES', 'SHOW VIEW', 'CREATE ROUTINE', 'ALTER ROUTINE', 'EXECUTE', 'EVENT', 'CREATE VIEW',
+    'SHOW DATABASES','FILE','PROCESSES', 'SHUTDOWN', 'SUPER'
+]
